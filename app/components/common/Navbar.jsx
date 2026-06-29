3f2086588e48f3666e5a0cdb76e8df1aaa0c1b1b
karan daksh <karan@quokkalabs.com>
2026-06-29 23:55:05 +0530

    Navbar Done of Page 8 Design HeroSection Pendding

--- app/components/common/Navbar.jsx
+++ app/components/common/Navbar.jsx
@@ -4,7 +4,7 @@ import React, { Children } from 'react'
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
-import logo from '@/public/assets/img/logo_orange.svg'
+import logo from '@/public/assets/img/logo_white.svg'
 import Button from '../ui/Button';
 
 
@@ -12,7 +12,8 @@ import Button from '../ui/Button';
 
 
 const menuItems = [
-  { id: 1, name: "Home", href: "/", group: "left", active: true },
+  // { id: 1, name: "Home", href: "/", group: "left", isActive: true },
+  { id: 1, name: "Home", href: "/", group: "left" },
   { id: 2, name: "Tour", href: "/tour", group: "left" },
   { id: 3, name: "Destination", href: "/destination", group: "left" },
   { id: 4, name: "Activity", href: "/activity", group: "left" },
@@ -25,7 +26,7 @@ const menuItems = [
 
 
 const Navbar = () => {
-  // const pathname = usePathname()
+  const pathname = usePathname()
   const OnClickHandle = () => {
     console.log('login Click');
 
@@ -35,40 +36,56 @@ const Navbar = () => {
 
   return (
     <>
+      <div className="transition-all duration-300 bg-transparent text-foreground shadow ">
 
-      <nav className="max-w-[1320px] mx-auto py-4 transition-colors duration-300">
-        <div className="mx-auto px-3.5">
-          <div className="grid grid-cols-12 items-center justify-between ms-8">
-            <div className="cursor-pointer col-span-2 -mt-1">
-              <Link href='/'> <img src={logo.src} alt="logo" /></Link>
-            </div>
-            <div className="leftMenu col-span-6">
-              {menuItems
-                .filter((item) => (item.group === 'left'))
-                .map((item) => (<Link key={item.id} href={item.href} className={`text-sm px-2 py-4 mx-2 ${item.active ? 'text-red-500' : ''}`}>{item.name}</Link>))
-              }
-            </div>
+        <nav className="max-w-[1320px] mx-auto py-4">
+          <div className="mx-auto px-3.5">
+            <div className="grid grid-cols-12 items-center justify-between ms-8">
+              <div className="cursor-pointer col-span-2 -mt-1">
+                <Link href='/'> <img src={logo.src} alt="logo" /></Link>
+              </div>
+              <div className="leftMenu col-span-6">
+                {menuItems
+                  .filter((item) => (item.group === 'left'))
+                  .map((item) => {
+                    const isActive = item.href === "/" ? pathname === "/" : pathname.startsWith(item.href) || pathname.includes(item.href);
+                    return (
+                      <Link key={item.id} href={item.href} className={`text-sm px-2 py-4 mx-2 ${isActive ? 'text-red-500' : ''} hover:text-red-500 transition-all duration-300`}>{item.name}</Link>
+                    )
+                  })
+                }
+              </div>
 
-            <div className="rightMenu col-span-4 text-right">
-              {menuItems
-                .filter((item) => (item.group === 'right'))
-                .map((item) => (<Link key={item.id} href={item.href} className='text-sm px-2 py-4 mx-2'>{item.name}</Link>))
-              }
+              <div className="rightMenu col-span-4 text-right">
+                {menuItems
+                  .filter((item) => (item.group === 'right'))
+                  .map((item) => {
 
-              
-              {/*<Button  btnType='button' btnClass='px-4 py-2 rounded-full border text-sm cursor-pointer' onClick={OnClickHandle}>Register</Button>*/}  {/* This is a for example this is not part of Menu */}
-              {/* <Button  btnType='button' btnClass='px-4 py-2 rounded-full border text-sm cursor-pointer' onMouseOver={OnClickHandle}>Login</Button> */}
-              
+                    const isActive = item.pathname === "/" ? pathname === "/" : pathname === pathname.startsWith(item.href) || pathname.includes(item.href)
+
+                    return (
+                      <Link key={item.id} href={item.href} className={`text-sm px-2 py-4 mx-2 ${isActive ? 'text-red-500' : ''} hover:text-red-500 transition-all duration-300`}>{item.name}</Link>
+                    )
+                  }
+
+                  )
+                }
+
+
+                {/*<Button  btnType='button' btnClass='px-4 py-2 rounded-full border text-sm cursor-pointer' onClick={OnClickHandle}>Register</Button>*/}  {/* This is a for example this is not part of Menu */}
+                {/* <Button  btnType='button' btnClass='px-4 py-2 rounded-full border text-sm cursor-pointer' onMouseOver={OnClickHandle}>Login</Button> */}
+
 
                 <Link href='/login' className='px-4 py-2 rounded-full border text-sm cursor-pointer'>Login</Link>
 
+              </div>
+
+
+
             </div>
-
-
-
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
 
 
     </>
---
+++ app/components/ui/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import herobg from '@/public/assets/img/hero_section_img.svg'
+
+
+const HeroSection = () => {
+    return (
+        <div className="heroSection flex justify-center items-center pt-8 pb-32">
+            <div>
+                <h1 className='text-7xl font-semibold pt-16 pb-3.5'>Find Next PlaceTo Visit</h1>
+                <p className='text-center text-sm font-reguler w-2/4 mx-auto'>Discover amzaing places at exclusive deals.Eat, Shop, Visit interesting places around the world.</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
